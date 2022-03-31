b4cdd006a64cda50f852d4a02e6bac73b1ba99f4
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-03-31 22:03:11 +0700

    add search empty tile func & compute X func

--- src/main.py
+++ src/main.py
@@ -2,4 +2,16 @@ fileName = input("Masukkan nama file: ")
 f = open(fileName, 'r')
 matriks = []
 matriks = [line.split() for line in f]
-print(matriks)
+
+def searchEmptyTile():
+    for row in range(4):
+        for col in range(4):
+            if (matriks[row][col] == '0'):
+                return (row, col)
+
+def computeX():
+    row, col = searchEmptyTile()
+    if ((row % 2 == 0 and col % 2 != 0) or (row % 2 != 0 and col % 2 == 0)):
+        return 1
+    else:
+        return 0
