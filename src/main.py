0846738418c01468bfdc4186d735539d206fa1e9
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-04-03 19:15:06 +0700

    add comment in main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 import os
 import os.path
 import tkinter as tk
-from turtle import update
-from time import sleep
 from matplotlib.pyplot import step
 from fifteenPuzzle import startSolve, getPath
-
 
 fileName = input("Masukkan nama file: ")
 
@@ -22,44 +19,37 @@ try:
         if os.path.exists(item + '\\' + fileName) and os.path.basename(os.path.normpath(item)) == 'test':
             file = item + '\\' + fileName
     f = open(file, 'r')
-except:
+except: # file tidak ditemukan
     print("File tidak ditemukan pada folder test")
     quit()
 
 matriks = [[int(elmt) for elmt in line.split()] for line in f] # ubah file ke dalam matriks
 finalNode = startSolve(matriks) # cari solusi puzzle
 
+# jika puzzle bisa dipecahkan, tampilkan UI
 if (finalNode != None):
     puzzleUI = tk.Tk(className = "15-Puzzle")
-
     puzzleUI.resizable(False, False)
 
-    flag = False
+    tiles = [] # array penampung label yang merepresentasikan tile pada UI
 
-    num = 1
-    tiles = []
-
+    # inisialisasi puzzle dengan keadaan awal
     for i in range(4):
         for j in range(4):
-            if (matriks[i][j] == 16):
+            if (matriks[i][j] == 16): # jika tile kosong
+                relief = "flat"
                 text = ''
+                bgColor = "#ffffff"
+                fgColor = "#ffffff"
             else:   
+                relief = "raised"
+                bgColor = "#247881"
+                fgColor = "#17252a"
                 text = str(matriks[i][j])
-            tile = tk.Label(puzzleUI, text = text, width = 4, height = 2, font=("Calibri", 30))
+            tile = tk.Label(puzzleUI, text = text, width = 4, height = 2, font=("Segoe UI Semibold", 30), bg= bgColor, fg = fgColor, relief = relief)
             tiles.append(tile)
-            num += 1
 
-    def resetPuzzle():
-        idx = 0
-        for i in range(4):
-            for j in range(4):
-                if (matriks[i][j] == 16):
-                    text = ''
-                else:
-                    text = str(matriks[i][j])
-                tiles[idx].config(text = text)
-                idx += 1
-
+    # posisikan tile dengan grid
     idx = 0
     for i in range(4):
         for j in range(4):
@@ -67,36 +57,70 @@ if (finalNode != None):
             idx += 1
 
     path = getPath(finalNode, matriks)
-    step = -1
+    step = -1 # counter untuk menghitung step yang sudah ditampilkan pada UI
+
+    def resetPuzzle():
+        '''
+            Mereset tampilan puzzle kembali ke tampilan awal
+        '''
+        idx = 0
+        for i in range(4):
+            for j in range(4):
+                if (matriks[i][j] == 16): # jika tile kosong
+                    relief = "flat"
+                    text = ''
+                    bgColor = "#ffffff"
+                    fgColor = "#ffffff"
+                else:
+                    relief = "raised"
+                    bgColor = "#247881"
+                    fgColor = "#17252a"
+                    text = str(matriks[i][j])
+                tiles[idx].config(text = text, background = bgColor, fg = fgColor, relief = relief)
+                idx += 1
 
     def startPuzzle():
+        '''
+            Prosedur untuk memulai menampilkan pergerakan puzzle pada UI
+        '''
         global step
-        if step > -1:
-            step = -1
+        if step > -1: 
+            step = -1 # reset step ke -1
         updatePuzzle()
         
     def updatePuzzle():
+        '''
+            Melakukan pergantian tampilan puzzle pada UI
+            sesuai dengan urutan pergerakan yang dilakukan
+        '''
         global step
-        startButton["state"] = "disabled"
-        if (step < len(path)):
+        startButton["state"] = "disabled" # disable tombol Mulai
+        if (step < len(path)): # jika belum mencapai step terakhir
             if step == -1:
                 resetPuzzle()
             else :
                 idx = 0
                 for i in range(4):
                     for j in range(4):
-                        if (path[step][1][i][j] == 16):
+                        if (path[step][1][i][j] == 16): # jika tile kosong
+                            relief = "flat"
                             text = ''
+                            bgColor = "#ffffff"
+                            bgColor = "#ffffff"
                         else:
+                            relief = "raised"
+                            bgColor = "#247881"
+                            fgColor = "#17252a"
                             text = str(path[step][1][i][j])
-                        tiles[idx].config(text = text)
+                        tiles[idx].config(text = text, background = bgColor, fg = fgColor, relief = relief)
                         idx += 1
             step += 1  
-            puzzleUI.after(1000, updatePuzzle)  
-        else:
-            startButton["state"] = "normal"    
+            puzzleUI.after(1000, updatePuzzle) # panggil kembali prosedur updatePuzzle setelah 1 detik
+        else: # pergerakan sudah selesai
+            startButton["state"] = "normal" # aktifkan kembali tombol Mulai
 
-    startButton = tk.Button(puzzleUI, text = "Mulai", command = startPuzzle)
-    startButton.grid(row = 4, column = 1, columnspan = 4, padx = 1, pady = 1)
+    # tambahkan tombol Mulai
+    startButton = tk.Button(puzzleUI, text = "Mulai", font=("Segoe UI Semibold", 12), command = startPuzzle, width = 5, height = 2) 
+    startButton.grid(row = 4, column = 1, columnspan = 2, padx = 1, pady = 1)
 
     puzzleUI.mainloop()
